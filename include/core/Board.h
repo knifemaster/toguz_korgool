e11bc527615ac5f7ab031fbb3ddd484043c54cfa
knifemaster <emilasanbekov@gmail.com>
2025-01-22 13:58:04 +0600

    added board class

---
+++ include/core/Board.h
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <type_info>
+
+enum class CellType {
+	TOGUZ_KORGOOL,
+	Type2,
+	Type3,
+	Type4,
+
+}
+
+
+class Board {
+
+	public:
+		Board(size_t size, int ball_count) {
+			player1.reserve(size);
+			player2.reserve(size);
+
+			for ()
+		
+		}
+		std::vector<int> get_board() {
+		
+		}
+
+	
+
+	private:
+		std::vector<int> player1;
+		std::vector<int> player2;	
+
+}
+
+
+int main() {
+
+	return 0;
+}
---
+++ include/core/Game.h
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <array>
+
+
+class Game {
+
+	public:
+		GameType game_type;
+
+		void move(Board, Player player2) {
+			
+		
+		}
+
+	private:
+		int rating;
+		
+		int game_id;
+
+}
+
+
+
+
+
+
+int main() {
+
+
+	return 0
+}
