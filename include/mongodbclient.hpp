9c94469e25c1d72b7ff305ae7924d39c0366f4f5
knifemaster <emilasanbekov@gmail.com>
2025-03-12 22:38:59 +0600

    added update function to mongodbclient

--- include/mongodbclient.hpp
+++ include/mongodbclient.hpp
@@ -44,3 +44,14 @@ public:
         }
     }
 
+    bool update_documents(const bsoncxx::document::view_or_value& filter, const bsoncxx::document::view_or_value& update) {
+        try {
+            auto result = collection_.update_many(filter, update);
+            std::cout << "Updated " << result->modified_count() << " documents." << std::endl;
+            return true;
+        } catch (const mongocxx::exception& e) {
+            std::cerr << "MongoDB error: " << e.what() << std::endl;
+            return false;
+        }
+    }
+
