fd379dcf89fc4f375a0cb2451ff34855ac05798b
knifemaster <emilasanbekov@gmail.com>
2025-03-12 22:33:49 +0600

    added header and includes to mongodb client

---
+++ include/mongodbclient.hpp
@@ -0,0 +1,9 @@
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/uri.hpp>
+#include <mongocxx/exception/exception.hpp>
+#include <bsoncxx/json.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <iostream>
+#include <string>
+
