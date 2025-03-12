bbd4d8e80d8691ca0c96ed0a7c48d729fb0b84ec
knifemaster <emilasanbekov@gmail.com>
2025-03-12 22:36:58 +0600

    added insert function to mongdbclient

--- include/mongodbclient.hpp
+++ include/mongodbclient.hpp
@@ -17,3 +17,18 @@ public:
     }
 
 
+        bool insert_document(const bsoncxx::document::view_or_value& document) {
+        try {
+            auto result = collection_.insert_one(document);
+            if (result) {
+                std::cout << "Document inserted with id: " << result->inserted_id().get_oid().value.to_string() << std::endl;
+                return true;
+            } else {
+                std::cerr << "Failed to insert document!" << std::endl;
+                return false;
+            }
+        } catch (const mongocxx::exception& e) {
+            std::cerr << "MongoDB error: " << e.what() << std::endl;
+            return false;
+        }
+    }
