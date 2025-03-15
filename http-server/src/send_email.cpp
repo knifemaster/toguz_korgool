7712562461f55100068c273fbfbf5976b18c803f
knifemaster <emilasanbekov@gmail.com>
2025-03-15 23:25:25 +0600

    added send_email part2

--- http-server/src/send_email.cpp
+++ http-server/src/send_email.cpp
@@ -11,3 +11,12 @@ int main() {
 
         curl_easy_setopt(curl, CURLOPT_MAIL_FROM, "e35213706@gmail.com");
 
+        struct curl_slist* recipients = NULL;
+        recipients = curl_slist_append(recipients, "e35213706@gmail.com");
+        curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
+
+        curl_easy_setopt(curl, CURLOPT_USERNAME, "fe35213706@gmail.com");
+        curl_easy_setopt(curl, CURLOPT_PASSWORD, "passwor");
+
+        curl_easy_setopt(curl, CURLOPT_USE_SSL, CURLUSESSL_ALL);
+
