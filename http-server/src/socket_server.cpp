6e4ca76c727127fffb24063677e342bedba6ac9a
knifemaster <emilasanbekov@gmail.com>
2025-03-28 18:07:52 +0600

    added include files

--- http-server/src/socket_server.cpp
+++ http-server/src/socket_server.cpp
@@ -1,32 +1,9 @@
 #include <iostream>
-#include <netinet/in.h>
-#include <sys/socket>
+#include <cstring>
 #include <unistd.h>
-
-
-using namespace std;
-
-
-int main() {
-
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-    sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(8080);
-    server_address.sin_addr.s_addr = INADDR_ANY;
-
-    bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address));
-
-    listen(server_socket, 5);
-
-    int client_socket = accept(server_socket, nullptr, nullptr);
-
-    char buffer[1024] = {0};
-    recv(client_socket, buffer, sizeof(buffer), 0);
-
-    cout << "Message from client : " << buffer << "\n";
-
-    close(server_socket);
-
-}
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <ctime>
+#include <string>
