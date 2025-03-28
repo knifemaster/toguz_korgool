088d3a2b0b3978735ae01995589769ff6f50cc0f
knifemaster <emilasanbekov@gmail.com>
2025-03-28 10:01:31 +0600

    creating server socket

---
+++ http-server/src/socket_server.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket>
+#include <unistd.h>
+
+
+using namespace std;
+
+
+int main() {
+
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+
+
+}
