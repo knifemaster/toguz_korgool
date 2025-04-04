91e121afbd1ba78227e1f37fb053bb00c664f2a9
knifemaster <emilasanbekov@gmail.com>
2025-04-04 17:36:10 +0600

    added client coroutine

--- http-server/src/echo_server_coro.cpp
+++ http-server/src/echo_server_coro.cpp
@@ -52,3 +52,60 @@ struct Task {
         void unhandled_exception() { std::terminate(); }
     };
 };
+
+
+// Обработка клиента с циклом сообщений
+Task client_coroutine(int epoll_fd, int client_fd) {
+    char buffer[1024] = {0};
+
+    while (true) {
+        // Ждём данные от клиента
+        co_await EpollAwaiter{epoll_fd, client_fd, EPOLLIN};
+
+        int valread = read(client_fd, buffer, sizeof(buffer) - 1);
+        if (valread <= 0) {
+            std::cout << "Client disconnected or read error.\n";
+            close(client_fd);
+            break;
+        }
+
+        buffer[valread] = '\0';
+        std::string input(buffer);
+        std::istringstream iss(input);
+        std::string command;
+        iss >> command;
+
+        std::string response;
+
+        if (command == "echo") {
+            std::string rest;
+            std::getline(iss, rest);
+            response = "Echo: " + rest + "\n";
+
+        } else if (command == "time") {
+            auto now = std::chrono::system_clock::now();
+            std::time_t t = std::chrono::system_clock::to_time_t(now);
+            response = "Time: " + std::string(std::ctime(&t));
+
+        } else if (command == "message") {
+            response = "Server says hello!\n";
+
+        } else if (command == "pow") {
+            double base, exp;
+            if (iss >> base >> exp) {
+                double result = std::pow(base, exp);
+                response = "Result: " + std::to_string(result) + "\n";
+            } else {
+                response = "Usage: pow <base> <exponent>\n";
+            }
+
+        } else {
+            response = "Unknown command\n";
+        }
+
+        send(client_fd, response.c_str(), response.size(), 0);
+    }
+
+    co_return;
+}
+
