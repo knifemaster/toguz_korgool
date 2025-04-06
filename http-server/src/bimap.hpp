2f05314275808dd428cdc009c42fc8d8b8542f20
knifemaster <emilasanbekov@gmail.com>
2025-04-07 01:13:56 +0600

    add insert method

--- http-server/src/bimap.hpp
+++ http-server/src/bimap.hpp
@@ -9,3 +9,10 @@ class Bimap {
     std::unordered_map<Left, Right> left_to_right;
     std::unordered_map<Right, Left> right_to_left;
     std::mutex bimap_mutex;
+
+public:
+    void insert(const Left& l, const Right& r) {
+        std::lock_guard<std::mutex> lock(bimap_mutex);
+        left_to_right[l] = r;
+        right_to_left[r] = l;
+    }
