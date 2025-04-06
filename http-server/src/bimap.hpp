17049e4c211c90ff9c363cf62c9a34e1fd279546
knifemaster <emilasanbekov@gmail.com>
2025-04-07 01:13:13 +0600

    add members of bimap

--- http-server/src/bimap.hpp
+++ http-server/src/bimap.hpp
@@ -3,3 +3,9 @@
 #include <stdexcept>
 #include <iostream>
 #include <mutex>
+
+template <typename Left, typename Right>
+class Bimap {
+    std::unordered_map<Left, Right> left_to_right;
+    std::unordered_map<Right, Left> right_to_left;
+    std::mutex bimap_mutex;
