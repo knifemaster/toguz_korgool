3c52939ecb2b3dbce2e2788f0a62518000dc3568
knifemaster <emilasanbekov@gmail.com>
2025-04-09 01:31:42 +0600

    added task

--- http-server/src/simple_server_coro.cpp
+++ http-server/src/simple_server_coro.cpp
@@ -56,3 +56,14 @@ struct EpollAwaiter {
 
     void await_resume() const noexcept {}
 };
+
+
+struct Task {
+    struct promise_type {
+        Task get_return_object() { return {}; }
+        std::suspend_never initial_suspend() { return {}; }
+        std::suspend_never final_suspend() noexcept { return {}; }
+        void return_void() {}
+        void unhandled_exception() { std::terminate(); }
+    };
+};
