6a0c47fbb91a2766b16f0f9433515bd924b313f2
knifemaster <emilasanbekov@gmail.com>
2025-04-09 01:28:52 +0600

    added include files for server

---
+++ http-server/src/simple_server_coro.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <coroutine>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <cstring>
+
+#include <sstream>
+#include <chrono>
+#include <ctime>
+#include <cmath>
+
+#include "match_maker.hpp"
+#include "game_manager2.hpp"
+
+#include "bimap.hpp"
