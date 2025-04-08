316e4519a514a8ba0ae7a4141ed92b3117fad66b
knifemaster <emilasanbekov@gmail.com>
2025-04-09 01:29:50 +0600

    add set_nonblocking and game_id generation

--- http-server/src/simple_server_coro.cpp
+++ http-server/src/simple_server_coro.cpp
@@ -16,3 +16,23 @@
 #include "game_manager2.hpp"
 
 #include "bimap.hpp"
+
+
+constexpr int PORT = 8080;
+constexpr int MAX_EVENTS = 10;
+
+
+std::atomic<int> game_id_count = 0;
+
+int generate_game_id() {
+    return game_id_count.fetch_add(1, std::memory_order_relaxed);
+}
+
+//std::unordered_map <int, Player>
+// Утилита для установки non-blocking сокета
+void set_nonblocking(int fd) {
+
+    int flags = fcntl(fd, F_GETFL, 0);
+    fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
+
