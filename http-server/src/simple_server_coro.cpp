6a491075705a9cdb268b85ac9b5380644227cfdd
knifemaster <emilasanbekov@gmail.com>
2025-04-11 14:48:48 +0600

    added client coroutine

--- http-server/src/simple_server_coro.cpp
+++ http-server/src/simple_server_coro.cpp
@@ -122,3 +122,121 @@ struct Task {
         void unhandled_exception() { std::terminate(); }
     };
 };
+
+
+// Обработка клиента с циклом сообщений
+Task client_coroutine(int epoll_fd, int client_fd, GameManager& manager, Bimap<int, int>& sockets_descriptors, ThreadSafeMatchmaker& matchmaker) {
+    char buffer[1024] = {0};
+
+    while (true) {
+        // Ждём данные от клиента
+        co_await EpollAwaiter{epoll_fd, client_fd, EPOLLIN};
+
+        int valread = read(client_fd, buffer, sizeof(buffer) - 1);
+        if (valread <= 0) {
+            std::cout << "Client disconnected or read error.\n";
+            close(client_fd);
+            break;
+        }
+
+        buffer[valread] = '\0';
+        std::string input(buffer);
+        std::istringstream iss(input);
+        std::string command;
+        iss >> command;
+
+        std::string response;
+        std::string response2;
+
+        if (command == "find_game") {
+            std::string id;
+            //std::getline(iss, id);
+            std::string rating;
+            //std::getline(iss, rating);
+            //int id_int = std::stoi(id);
+            //int rating_int = std::stoi(rating);
+
+            int id_int, rating_int;
+            if (iss >> id_int >> rating_int) {
+                std::cout << "id:" << id_int << std::endl;
+                std::cout << "rating:" << rating_int << std::endl;
+
+            }
+
+   
+
+        //matchmaker
+        //matchmaker.addPlayer
+            matchmaker.addPlayer({
+                client_fd,
+                id_int,
+                rating_int,
+                std::chrono::system_clock::now()
+            });
+
+            std::pair<Player, Player> match;
+            for (const auto& player : matchmaker.get_players()) {
+                std::cout << player.client_fd << " " << player.id << " " << player.rating << std::endl;    
+            }
+            
+            if (matchmaker.tryFindMatch(match)) {
+                std::cout << "in matchmaker if" << std::endl;
+                // нашли игроков
+                
+
+                std::cout << match.first.id << " " << match.second.id << std::endl;
+                std::cout << match.first.rating << " " << match.second.rating << std::endl;
+                response = "game finded id " + std::to_string(match.first.client_fd);
+                response2 = "game finded id " + std::to_string(match.second.client_fd);
+
+                //manager.create_game(generate_game_id());
+                // resend game_id
+                // resend color of players
+                // 
+                send(match.first.client_fd, response.c_str(), response.size(), 0);
+                send(match.second.client_fd, response2.c_str(), response2.size(), 0);
+                //
+                // В реальном приложении здесь была бы логика старта игры
+                std::cout << "end of matchmaker if" << std::endl;
+                continue;
+            }
+            //matchmaker.matchingThread();
+            
+           response = "in queue";
+       
+        // finding games and add player id
+        // and remember socket ids
+        // remeber game_id
+        }
+
+        if (command == "echo") {
+            std::string rest;
+            std::getline(iss, rest);
+            response = "Echo: " + rest + "\n";
+
+        } else if (command == "time") {
+            auto now = std::chrono::system_clock::now();
+            std::time_t t = std::chrono::system_clock::to_time_t(now);
+            response = "Time: " + std::string(std::ctime(&t));
+
+        } else if (command == "message") {
+            response = "Server says hello!\n";
+
+        } else if (command == "pow") {
+            double base, exp;
+            if (iss >> base >> exp) {
+                double result = std::pow(base, exp);
+                response = "Result: " + std::to_string(result) + "\n";
+            } else {
+                response = "Usage: pow <base> <exponent>\n";
+            }
+
+        }// else {
+         //   response = "Unknown command\n";
+        //}
+
+        send(client_fd, response.c_str(), response.size(), 0);
+    }
+
+    co_return;
+}
