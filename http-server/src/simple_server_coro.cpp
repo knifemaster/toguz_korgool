b78f38df25fc07245d8d5bc6d7733e4da6aa4000
knifemaster <emilasanbekov@gmail.com>
2025-04-15 19:07:23 +0600

    added set_nonblocking and generate_game_id function

--- http-server/src/simple_server_coro.cpp
+++ http-server/src/simple_server_coro.cpp
@@ -24,3 +24,19 @@
 #include "game_manager2.hpp"
 #include "bimap.hpp"
 
+constexpr int PORT = 8080;
+constexpr int MAX_EVENTS = 10;
+
+std::atomic<int> game_id_count = 0;
+
+int generate_game_id() {
+    return game_id_count.fetch_add(1, std::memory_order_relaxed);
+}
+
+void set_nonblocking(int fd) {
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags == -1 || fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+        throw std::runtime_error("Failed to set non-blocking mode");
+    }
+}
+
