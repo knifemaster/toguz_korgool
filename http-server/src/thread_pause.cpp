6ed788e1e6ab947bd12509ebc65369e8db454b08
knifemaster <emilasanbekov@gmail.com>
2025-05-08 17:45:38 +0600

    added ThreadPool class with queue tasks

--- http-server/src/thread_pause.cpp
+++ http-server/src/thread_pause.cpp
@@ -3,6 +3,9 @@
 #include <mutex>
 #include <condition_variable>
 #include <atomic>
+#include <vector>
+#include <queue>
+#include <functional>
 
 
 class PausableThread {
@@ -75,7 +78,60 @@ private:
 };
 
 
+class ThreadPool {
+    public:
+        ThreadPool(size_t num_threads) {
+            for (size_t i = 0; i < num_threads; ++i) {
+                workers.emplace_back([this] {
+                    while (true) {
+                        std::function<void()> task; 
+                        {
+                            std::unique_lock<std::mutex> lock(queue_mutex);
+                            condition.wait(lock, [this] { return !tasks.empty() || stop; });
+
+                            if (stop && tasks.empty()) return;
+
+                            task = std::move(tasks.front());
+                            tasks.pop();
+                        }
+                        task();
+                    }        
+                });
+            }
+        }
+
+        template <class F>
+        void enqueue(F&& f) {
+            {
+                std::unique_lock<std::mutex> lock(queue_mutex);
+                tasks.emplace(std::forward<F>(f));
+            }
+            condition.notify_one();
+        }
+
+        ~ThreadPool() {
+            {
+                std::unique_lock<std::mutex> lock(queue_mutex);
+                stop = true;
+            }
+            condition.notify_all();
+            for (std::thread & worker : workers) {
+                worker.join();
+            }
+        
+        }
+
+    private:
+        std::vector<std::thread> workers;
+        std::queue<std::function<void()>> tasks;
+        std::mutex queue_mutex;
+        std::condition_variable condition;
+        bool stop = false;
+};
+
+
 int main() {
+
     PausableThread thread;
     thread.start();
 
